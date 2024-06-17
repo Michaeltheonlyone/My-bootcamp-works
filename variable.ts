d3fe89bd19831fc3ab8ac319e4e6e8bfd81240a0
Michael <jeremiahchimaobi7@gmail.com>
2024-06-17 12:55:55 +0100

    Add variables and palindrome

---
+++ variable.ts
@@ -0,0 +1,8 @@
+console.log("............Const..........")
+let myName = "Michael";
+myName = "Micky";
+console.log(myName)
+console.log("........LET.........")
+let myName2 = "Michael"
+myName2 = "Micky"
+console.log(myName2)
