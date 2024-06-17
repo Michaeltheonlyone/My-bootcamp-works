287b38e82be8592f30dd306bccd561488b4b4aa8
Michael <jeremiahchimaobi7@gmail.com>
2024-06-17 13:29:30 +0100

    Add more data-types

---
+++ data-types.ts
@@ -0,0 +1,21 @@
+const shoppinglistMen = ["boxers" , "earrings" , "shoes" , "perfume" , "make up box"]
+console.log(".......Accessing Array.......")
+console.log(shoppinglistMen[3])
+console.log(shoppinglistMen.length)
+
+// DICTIONARY (OBJECT)
+// key value pairs
+// curly braces
+
+const contact_details = {
+    firstname: "Elekwa",
+    lastname: "Michael",
+    email: "jeremiahchimaobi7@gmail.com",
+    Tel: "+22995427908",
+    address: "C/482 Jericho, Cotonou",
+    country: "Benin" ,
+}         
+
+console.log(contact_details.email)
+console.log(contact_details["country"])
+console.log(contact_details.lastname)
